54120a34acde7995bf627fd090be968d057400cd
Sandra Hoyos <slhoyosr@gmail.com>
2022-04-14 23:42:03 -0500

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-unused-vars
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._currency = currency;
+    this._amount = amount;
+  }
+
+  // getter
+  get currency() {
+    return this._currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  // setter
+  set currency(Currency) {
+    this._currency = Currency;
+  }
+
+  set amount(Amount) {
+    this._amount = Amount;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
