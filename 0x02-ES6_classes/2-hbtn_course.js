ce415734a9cee21d996c41aec1edf87da768c6a8
Sandra Hoyos <slhoyosr@gmail.com>
2022-04-14 22:21:38 -0500

    Constructor, getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = typeof name === 'string' ? this._name = name : () => { throw TypeError('Name must be a string'); };
-    this._length = typeof length === 'number' ? this._length = length : () => { throw TypeError('Length must be a number'); };
-    this._students = (Array.isArray(students) && students.every((element) => typeof element === 'string')) ? this._students = students : () => { throw TypeError('Students must be an array of strings'); };
+    this._name = typeof name === 'string' ? name : () => { throw TypeError('Name must be a string'); };
+    this._length = typeof length === 'number' ? length : () => { throw TypeError('Length must be a number'); };
+    this._students = (Array.isArray(students) && (students.every((element) => typeof element === 'string'))) ? students : () => { throw TypeError('Students must be an array of strings'); };
   }
   // getter
 
@@ -20,14 +20,14 @@ export default class HolbertonCourse {
 
   // Setter
   set name(name) {
-    this._name = typeof name === 'string' ? this._name = name : () => { throw TypeError('Name must be a string'); };
+    this._name = typeof name === 'string' ? name : () => { throw TypeError('Name must be a string'); };
   }
 
   set length(length) {
-    this._length = typeof length === 'number' ? this._length = length : () => { throw TypeError('Length must be a number'); };
+    this._length = typeof length === 'number' ? length : () => { throw TypeError('Length must be a number'); };
   }
 
   set students(students) {
-    this._students = (Array.isArray(students) && students.every((element) => typeof element === 'string')) ? this._students = students : () => { throw TypeError('Students must be an array of strings'); };
+    this._students = (Array.isArray(students) && (students.every((element) => typeof element === 'string'))) ? students : () => { throw TypeError('Students must be an array of strings'); };
   }
 }
