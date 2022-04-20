86d43910f6df0f5b300e1154ab0d1700bf142ff5
Sandra Hoyos <slhoyosr@gmail.com>
2022-04-19 21:31:59 -0500

    cleanset

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length) { 
-      return ''; }
+  if (!startString || !startString.length) {
+    return '';
+  }
   let resultado = '';
   set.forEach((element) => {
-    if (element && element.startsWith(startString)) { resultado += `${element.slice(startString.length)}-`; }
+    if (element && element.startsWith(startString)) {
+      resultado += `${element.slice(startString.length)}-`;
+    }
   });
 
   return resultado.slice(0, resultado.length - 1);
